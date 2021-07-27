a7e7012697ce253d3053e6db3b7593ea163b91a7
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-07-27 18:33:32 +0400

    Change numbers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 150;
-        int count = 5;
-        int total = price * count;
+        int current_balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int total = current_balance * remittance;
         System.out.println(total);
     }
 }
