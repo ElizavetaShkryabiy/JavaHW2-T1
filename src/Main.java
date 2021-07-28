6ced1c1e627f08e096185c70bcf65571fff04223
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-07-28 17:26:30 +0400

    add issue

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int current_balance = 2_000_000_000;
-        int remittance = 500_000_000;
+        int remittance = 147_483_651;
         int total = current_balance + remittance;
         System.out.println(total);
     }
