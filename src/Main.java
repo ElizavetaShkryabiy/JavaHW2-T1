8a2cb03cf2e16714ce67db9077b5175ea17eb81f
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-07-29 16:11:01 +0400

    errors fixed

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance = 2_000_000_000;
+        int currentBalance = 2_000_000_000;
         int remittance = 147_483_651;
-        int total = current_balance + remittance;
+        int total = currentBalance + remittance;
         System.out.println(total);
     }
 }
